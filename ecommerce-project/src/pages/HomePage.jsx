031d7c278fb7d16a4d710b046c9e7cae9d97b3ba
Gagn-kainth <preetgagan710@gmail.com>
2026-06-13 00:17:55 +0530

    Add server proxy config for /api and/images

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -9,12 +9,12 @@ function HomePage() {
   const[cart ,setCart]=useState([])
 
   useEffect(() =>{
-    axios.get('http://localhost:3000/api/products')
+    axios.get('/api/products')
       .then((Response)=>{
         setProducts(Response.data)
     });
 
-    axios.get('http://localhost:3000/api/cart-items')
+    axios.get('/api/cart-items')
     .then((Response)=>{
       setCart(Response.data)
     })
