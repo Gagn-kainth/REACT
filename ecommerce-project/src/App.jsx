ee42f49ba8d2cfc1c22d149eb14adb426c243fb6
Gagn-kainth <preetgagan710@gmail.com>
2026-06-12 13:53:50 +0530

    Moves orders page and tracking page to React

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,18 +1,20 @@
 import CheckoutPage from "./pages/CheckoutPage";
-import HomePage from "./pages/HomePage"
-import { Routes,Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import { Routes, Route } from "react-router";
+import OrdersPage from "./pages/OrdersPage";
+import TrackingPage from "./pages/TrackingPage";
 
 function App() {
-  
   return (
     <>
-    <Routes>
-      <Route index element={<HomePage/>}/>
-      <Route path="checkout" element={<CheckoutPage/>}/>
-    </Routes>
-
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="checkout" element={<CheckoutPage />} />
+        <Route path="orders" element={<OrdersPage />} />
+        <Route path="tracking" element={< TrackingPage/>}/>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
